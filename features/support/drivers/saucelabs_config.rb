ff9d5c8b74cb00c25b6a8ee8cf4d18cdc057ed22
johannes112 <johannes.emmos@gmail.com>
2016-09-21 13:40:01 +0000

    extension: saucelabs_config.rb --> modified browser but ios donot work

--- features/support/drivers/saucelabs_config.rb
+++ features/support/drivers/saucelabs_config.rb
@@ -1,4 +1,73 @@
 if ENV['DRIVER'] == 'saucelabs'
+  require 'capybara/cucumber'
+  module Saucelabs_config
+    Capybara.default_selector = :css
+    Capybara.default_driver = :sauce
+    Capybara.ignore_hidden_elements = true
+    Capybara.default_max_wait_time = 30
+
+    if ENV['BROWSER'] == 'firefox'
+      @caps = {
+        :platform => "Windows 7",
+        :browserName => "Firefox",
+        :version => "40"
+        }
+    elsif ENV['BROWSER'] == 'ie'
+      @caps = {
+        :platform => "Windows 8",
+        :browserName => "Internet Explorer",
+        :version => "10"
+        }
+    elsif ENV['BROWSER'] == 'chrome'
+      @caps = {
+        :platform => "Windows 7",
+        :browserName => "Chrome",
+        :version => "40"
+        }
+    elsif ENV['BROWSER'] == 'safari'
+        @caps = {
+        :platform => "OS X 10.10",
+        :browserName => "Safari",
+        :version => "8.0"
+        }
+    elsif ENV['BROWSER'] == 'safari'
+        @caps = {
+        :platform => "OS X 10.10",
+        :browserName => "Safari",
+        :version => "8.0"
+        }        
+    elsif ENV[BROWSER == 'ios']
+        @caps = {
+          :browserName => 'Safari',
+          :appiumVersion => '1.5.2',
+          :deviceName => "iPhone 6",
+          :deviceOrientation => "portrait",
+          :platformName => 'iOS',
+          :platformVersion => '9.2',
+          :name => "iPhone 6"
+        }
+    end
+    
+    @url_path = "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com:80/wd/hub"
+   
+    Capybara.register_driver :saucelabs do |app|
+      Capybara::Selenium::Driver.new(app, :browser => :remote, :url => @url_path, :desired_capabilities => @caps)
+    end
+    
+    Capybara.default_driver = :saucelabs
+    
+=begin    Capybara.register_driver(:appium) do |app|
+      Appium::Capybara::Driver.new(app, :browser => :remote, :url => @url_path, :desired_capabilities => @caps)
+    end
+    
+    Capybara.default_driver = :appium
+=end
+  end
+end
+#driver.quit()
+
+=begin
+if ENV['DRIVER'] == 'saucelabs'
   require 'sauce/capybara'
   require 'capybara/cucumber'
   module Saucelabs_config
@@ -34,6 +103,12 @@ if ENV['DRIVER'] == 'saucelabs'
         ["OS X 10.10", "Safari", "8.0"]
         ]
       end
+      if ENV['BROWSER'] == 'ios'
+        config[:browsers] = [
+        ['OS X 10.10', 'iphone', '9.1', {device_orientation: 'portrait'}],
+        ]
+      end
     end
   end
 end
+=end
