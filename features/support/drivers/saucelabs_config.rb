5d5d8f5df748a72e87eda2186fa2ee7eb2775c29
johannes112 <johannes.emmos@gmail.com>
2016-09-21 14:29:23 +0000

    extension: saucelabs_config.rb --> modified browser v3

--- features/support/drivers/saucelabs_config.rb
+++ features/support/drivers/saucelabs_config.rb
@@ -10,36 +10,30 @@ if ENV['DRIVER'] == 'saucelabs'
       @caps = {
         :platform => "Windows 7",
         :browserName => "Firefox",
-        :version => "40"
+        :version => "latest"
         }
     elsif ENV['BROWSER'] == 'ie'
       @caps = {
         :platform => "Windows 8",
         :browserName => "Internet Explorer",
-        :version => "10"
+        :version => "latest"
         }
     elsif ENV['BROWSER'] == 'chrome'
       @caps = {
         :platform => "Windows 7",
         :browserName => "Chrome",
-        :version => "40"
+        :version => "latest"
         }
     elsif ENV['BROWSER'] == 'safari'
         @caps = {
         :platform => "OS X 10.10",
         :browserName => "Safari",
-        :version => "8.0"
+        :version => "latest"
         }
-    elsif ENV['BROWSER'] == 'safari'
-        @caps = {
-        :platform => "OS X 10.10",
-        :browserName => "Safari",
-        :version => "8.0"
-        }        
-    elsif ENV[BROWSER == 'ios']
+    elsif ENV['BROWSER'] == 'iPhone'
         @caps = {
           :browserName => 'Safari',
-          :appiumVersion => '1.5.2',
+          :appiumVersion => '1.5.3',
           :deviceName => "iPhone 6",
           :deviceOrientation => "portrait",
           :platformName => 'iOS',
@@ -56,59 +50,7 @@ if ENV['DRIVER'] == 'saucelabs'
     
     Capybara.default_driver = :saucelabs
     
-=begin    Capybara.register_driver(:appium) do |app|
-      Appium::Capybara::Driver.new(app, :browser => :remote, :url => @url_path, :desired_capabilities => @caps)
-    end
-    
-    Capybara.default_driver = :appium
-=end
   end
 end
 #driver.quit()
 
-=begin
-if ENV['DRIVER'] == 'saucelabs'
-  require 'sauce/capybara'
-  require 'capybara/cucumber'
-  module Saucelabs_config
-    Capybara.default_selector = :css
-    Capybara.default_driver = :sauce
-    Capybara.ignore_hidden_elements = true
-    Capybara.default_max_wait_time = 30
-    
-    Sauce.config do |config|
-      config[:name] = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']}"
-      config[:start_tunnel] = false
-
-      if ENV['BROWSER'] == 'firefox'
-        config['screen-resolution'] = "1280x960"
-        config[:browsers] = [
-          ["Windows 7", "Firefox", "40"]
-          ]
-      end
-      if ENV['BROWSER'] == 'ie'
-        config['screen-resolution'] = "1280x960"
-        config[:browsers] = [
-        ["Windows 7", "Internet Explorer", "10"]
-        ]
-      end
-      if ENV['BROWSER'] == 'chrome'
-        config['screen-resolution'] = "1280x960"
-        config[:browsers] = [
-        ["Windows 7", "Chrome", "40"]
-        ]
-      end
-      if ENV['BROWSER'] == 'safari'
-        config[:browsers] = [
-        ["OS X 10.10", "Safari", "8.0"]
-        ]
-      end
-      if ENV['BROWSER'] == 'ios'
-        config[:browsers] = [
-        ['OS X 10.10', 'iphone', '9.1', {device_orientation: 'portrait'}],
-        ]
-      end
-    end
-  end
-end
-=end
