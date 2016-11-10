983753b63046fccc4ae148c01428fb06aed7a004
Johannes Launer <johannes.emmos@gmail.com>
2016-11-10 10:28:05 +0000

    wrote new step for search on mobile devices

--- features/support/drivers/saucelabs_config.rb
+++ features/support/drivers/saucelabs_config.rb
@@ -1,10 +1,13 @@
 if ENV['DRIVER'] == 'saucelabs'
+  require 'sauce/capybara'
   require 'capybara/cucumber'
   module Saucelabs_config
     Capybara.default_selector = :css
+    Capybara.default_driver = :sauce
     Capybara.ignore_hidden_elements = true
     Capybara.default_max_wait_time = 30
     
+<<<<<<< HEAD
     if ENV['BROWSER'] == 'firefox'
       @caps = {
         :platform => "Windows 7",
@@ -63,5 +66,35 @@ if ENV['DRIVER'] == 'saucelabs'
       Capybara::Selenium::Driver.new(app, :browser => :remote, :url => @url_path, :desired_capabilities => @caps)
     end
     Capybara.default_driver = :saucelabs_driver
+=======
+    Sauce.config do |config|
+      config[:name] = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']}"
+      config[:start_tunnel] = false
+
+      if ENV['BROWSER'] == 'firefox'
+        config['screen-resolution'] = "1280x960"
+        config[:browsers] = [
+          ["Windows 7", "Firefox", "40"]
+          ]
+      end
+      if ENV['BROWSER'] == 'ie'
+        config['screen-resolution'] = "1280x960"
+        config[:browsers] = [
+        ["Windows 7", "Internet Explorer", "10"]
+        ]
+      end
+      if ENV['BROWSER'] == 'chrome'
+        config['screen-resolution'] = "1280x960"
+        config[:browsers] = [
+        ["Windows 7", "Chrome", "40"]
+        ]
+      end
+      if ENV['BROWSER'] == 'safari'
+        config[:browsers] = [
+        ["OS X 10.10", "Safari", "8.0"]
+        ]
+      end
+    end
+>>>>>>> parent of 65ac91f... fix: use ruby logic for saucelabs_config.rb, problem was the newest version of firefox use a webdriver that not supports all actions
   end
 end
