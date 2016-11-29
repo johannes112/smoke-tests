058caed4570027297a4ffc127338273395c6b608
Johannes Launer <johannes.emmos@gmail.com>
2016-11-29 10:25:18 +0000

    added default values and folder for vega, hotelwaesche, jobeline by copy&paste

---
+++ features/extension/shops/jobeline/Settings.rb
@@ -0,0 +1,65 @@
+#pulsiva
+class Settings
+  attr_accessor :url_checkout_confirm, :url_add_comand, :urlHttp, :urlHttps, :urlBackend, :baby_steps
+  
+  def initialize
+    if ENV['SYSTEM'] == "int"
+      @urlHttp = case ENV['COUNTRY']
+        when (/de/) then 'http://int.pulsiva.com/de-de/'
+        when (/ch/) then 'http://int.pulsiva.com/at-de/'
+        when (/at/) then 'http://int.pulsiva.com/ch-de/'
+        when (/it/) then 'http://int.pulsiva.com/it-it/'
+        when (/fr/) then 'http://int.pulsiva.com/fr-fr/'
+        when (/no/) then 'http://int.pulsiva.com/no-no/'
+        when (/se/) then 'http://int.pulsiva.com/se-se/'
+        when (/nl/) then 'http://int.pulsiva.com/nl-nl/'
+        when (/be/) then 'http://int.pulsiva.com/be-fr/'
+        when (/es/) then 'http://int.pulsiva.com/es-es/'
+      end
+      @urlHttps = case ENV['COUNTRY']#Important:write 's
+        when (/de/) then 'https://int.pulsiva.com/de-de/'
+        when (/at/) then 'https://int.pulsiva.com/at-de/'
+        when (/ch/) then 'https://int.pulsiva.com/ch-de/'
+        when (/it/) then 'https://int.pulsiva.com/it-it/'
+        when (/fr/) then 'https://int.pulsiva.com/fr-fr/'
+        when (/no/) then 'https://int.pulsiva.com/no-no/'
+        when (/se/) then 'https://int.pulsiva.com/se-se/'
+        when (/nl/) then 'https://int.pulsiva.com/nl-nl/'
+        when (/be/) then 'https://int.pulsiva.com/be-fr/'
+        when (/es/) then 'https://int.pulsiva.com/es-es/'
+        end
+      @urlBackend = 'https://int.pulsiva.com/'
+    end
+    
+    if ENV['SYSTEM'] == "live"
+      @urlHttp = case ENV['COUNTRY']
+        when (/de/) then 'http://www.pulsiva.com/de-de/'
+        when (/at/) then 'http://www.pulsiva.com/at-de/'
+        when (/ch/) then 'http://www.pulsiva.com/ch-de/'
+        when (/it/) then 'http://www.pulsiva.com/it-it/'
+        when (/fr/) then 'http://www.pulsiva.com/fr-fr/'
+        when (/no/) then 'http://www.pulsiva.com/no-no/'
+        when (/se/) then 'http://www.pulsiva.com/se-se/'
+        when (/nl/) then 'http://www.pulsiva.com/nl-nl/'
+        when (/be/) then 'http://www.pulsiva.com/be-fr/'
+        when (/es/) then 'http://www.pulsiva.com/es-es/'
+      end
+      @urlHttps = case ENV['COUNTRY']
+        when (/de/) then 'https://www.pulsiva.com/de-de/'
+        when (/at/) then 'https://www.pulsiva.com/at-de/'
+        when (/ch/) then 'https://www.pulsiva.com/ch-de/'
+        when (/it/) then 'https://www.pulsiva.com/it-it/'
+        when (/fr/) then 'https://www.pulsiva.com/fr-fr/'
+        when (/no/) then 'https://www.pulsiva.com/no-no/'
+        when (/se/) then 'https://www.pulsiva.com/se-se/'
+        when (/nl/) then 'https://www.pulsiva.com/nl-nl/'
+        when (/be/) then 'https://www.pulsiva.com/be-fr/'
+        when (/es/) then 'https://www.pulsiva.com/es-es/'
+      end
+      @urlBackend = 'https://www.pulsiva.com/'
+    end
+    @url_checkout_confirm = "checkout/shippingPayment"
+    @url_add_comand = 'direktbestellen'
+    @baby_steps = true
+  end
+end
