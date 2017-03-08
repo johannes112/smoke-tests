9fde91567dad1f25f92485dc0bd71d78649916fc
Johannes Launer <johannes.emmos@gmail.com>
2017-03-08 08:59:45 +0000

    fixing: replaced api-comand with manual babysteps as it was at the beginning (reverted)

--- features/support/mechanize/demo_mechanize.rb
+++
@@ -1,5 +0,0 @@
-require_relative "url_functions.rb"
-  url_functions = UrlFunctions.new
-  url_functions.set_url('https://www.vega-direct.com/de-de/')
-  puts url_functions.check_all_server
-  #url_functions.check_all_server
