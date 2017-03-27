72a364213684ac9b6df5a584924855156691b3b9
Johannes Launer <johannes.emmos@gmail.com>
2017-03-27 14:03:18 +0000

    extension: for debuging added commented Timecomands

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,19 +1,19 @@
 Before do
+  #@start_time = Time.now
   $start ||= false
   if !$start 
+    #puts "Time: #{@start_time.hour.to_s}:#{@start_time.min.to_s}:#{@start_time.sec.to_s}"
     # if browser is for a desktop then resize dimension otherwise some windows could distrub the flow
     if ENV['BROWSER'] == 'iPad' || ENV['BROWSER'] == 'iPhone'
       puts "Test mobile"
     else
       page.driver.browser.manage.window.size = Selenium::WebDriver::Dimension.new(1920, 1200)#1280x1024
     end
-    #puts "current_driver:#{@driver}"
     $start = true 
   end
 end
 
 at_exit() do
-  #Capybara.current_session.driver.quit
   #if $failed_scenarios
   #  puts "\033[35m#{$failed_scenarios.size} scenario failed: ==> #{$failed_scenarios}"
   #end
@@ -21,10 +21,12 @@ at_exit() do
   Capybara.reset_sessions!
   Capybara.current_session.driver.quit
   Capybara.use_default_driver
+  #puts_time_elapsed(Time.now)
   #Environment.debug?
 end
 
 After do |s| 
+  #puts_time_elapsed
   # Tell Cucumber to quit after this scenario is done - if it failed.
   #Cucumber.wants_to_quit = true if s.failed?
   #if s.failed?
