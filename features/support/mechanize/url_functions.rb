a8c141d764bd99d557e0f6d2a239ca71a8dcee26
Johannes Launer <johannes.emmos@gmail.com>
2017-05-29 12:52:10 +0000

    bugfix: check_header(blabla) -> removed puts

--- features/support/mechanize/url_functions.rb
+++ features/support/mechanize/url_functions.rb
@@ -28,7 +28,6 @@ class UrlFunctions
       header_value_x_made_on = page[header_x_made_on]
       push_to_server_arr(header_value_x_made_on)
     else
-      puts "I am on #{page.href}"
       puts "There is no key '#{header_x_made_on}'"
       puts "Header all keys: #{page.header.keys}"
       header_value_x_made_on = 0
