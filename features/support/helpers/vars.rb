15d835125a1900d24f3eb9ca30f3921b02e38246
johannes112 <johannes.emmos@gmail.com>
2018-02-05 16:52:09 +0100

    code: added var for paymentmethods

--- features/support/helpers/vars.rb
+++ features/support/helpers/vars.rb
@@ -1,18 +1,19 @@
 class Vars
-  attr_accessor :r_system, :r_country, :r_shop, :url_productpage, :url_productcart, :url_account
-  
+  attr_accessor :r_system, :r_country, :r_shop, :url_productpage, :url_productcart, :url_account, :paymentmethods
+
   def initialize (r_system='dual', r_country='eu', r_shop='veg')
     @r_system = r_system
     @r_country = r_country
     @r_shop = r_shop
+    self.paymentmethods = []
   end
-  
+
   def output_var
     puts "r_system:#{r_system}"
     puts "r_country:#{r_country}"
     puts "r_shop:#{r_shop}"
   end
-  
+
   def pulsiva?
     bool = false
     if @r_shop.match(/pulsiva/)
@@ -21,7 +22,7 @@ class Vars
     end
     return bool
   end
-  
+
 end
 
 VARS_ENV = Vars.new(ENV['SYSTEM'], ENV['COUNTRY'], ENV['SHOP'])
