ac928eaa92cd44beb23b1ea88c4b8bd65bce48d7
johannes112 <johannes.emmos@gmail.com>
2018-03-16 12:23:39 +0100

    quickfix: content -> server have to be 4

--- features/step_definitions/content.rb
+++ features/step_definitions/content.rb
@@ -3,7 +3,7 @@ When(/^I check the output of all servers of different webpage by visiting some l
   if ENV['SYSTEM'] == 'live'
     url_functions.set_url(settings.urlHttps)
     server_status_value = url_functions.check_all_server
-    expect(server_status_value).to eq("3"),
+    expect(server_status_value).to eq("4"),
        "expect to get an ok but not all server working faultless. \nError message: #{server_status_value}"
     puts "amount of checked servers:#{server_status_value}"
   else
