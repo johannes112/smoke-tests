fdd8e5b2b2d49199a01780d0818ad75e83b51820
johannes launer <johannes.emmos@gmail.com>
2018-05-25 05:05:18 +0000

    config saucelabs: split video of job and set status for each scenario

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'headless', '2.3.1'
+gem 'headless', '2.3.0'
 gem 'cucumber', '3.1.0'#, '2.0.0'
 gem "capybara", '2.18.0'#, '2.7.1'
 gem "rspec", '3.7.0'#, '3.5.0'
 gem "selenium-webdriver", '3.9.0'#, '2.53.4'
+gem 'sauce_whisk', '0.1.0'
 gem 'httparty', '0.16.0'#, '0.14.0'
 gem 'mechanize', '2.7.5'
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,7 @@
+require "capybara/cucumber"
+require "selenium/webdriver"
+require 'sauce_whisk'
+
 Before('@ignore') do | s |
   # This will only run before scenarios tagged
   @skipped_scenarios = Array.new
@@ -27,13 +31,26 @@ end
 at_exit() do
   puts "\033[42mReset whole session and quit driver\033[0m\n"
   Capybara.reset_sessions!
-  #$driver.quit
+  Capybara.current_session.driver.quit
   #Capybara.current_session.driver.quit
   #Capybara.use_default_driver
   #Environment.debug?
 end
 
-#After do |s|
+# After do |s|
+  #set job status : Success|Failed
+  
+  # sessionid = ::Capybara.current_session.driver.browser.session_id
+  # jobid = SauceWhisk::Jobs.fetch(sessionid)
+  # ::Capybara.current_session.driver.quit
+  # if s.exception
+  #   SauceWhisk::Jobs.fail_job sessionid
+  # else
+  #   SauceWhisk::Jobs.pass_job sessionid
+  # end
+  #puts "\033[42mReset whole session and quit driver\033[0m\n"
+  #Capybara.reset_sessions!
+  
   #puts_time_elapsed
   # Tell Cucumber to quit after this scenario is done  if it failed.
   #Cucumber.wants_to_quit = true if s.failed?
@@ -48,3 +65,21 @@ end
     #run each failed scenarios again with command rerun this is useless / dreprecated
 #   end
 # end
+
+#with this around there will be a video for each scenario but this takes too much time
+Around do |scenario, block|
+  block.call
+  #get id of job
+  sessionid = ::Capybara.current_session.driver.browser.session_id
+  ::Capybara.current_session.driver.quit
+  #get last tag-name
+  scenario_tag = scenario.source_tag_names.last
+  env_vars = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']} #{ENV['JOB_NAME']}__#{ENV['BUILD_NUMBER']}"
+  jobname = "#{env_vars} - #{scenario_tag}"
+  # jobname = "#{scenario.feature.name} - #{scenario.name} - #{scenario_tag}"
+  
+  job = SauceWhisk::Jobs.fetch sessionid
+  job.name = jobname
+  job.passed = scenario.passed? ? true : false
+  job.save
+end
