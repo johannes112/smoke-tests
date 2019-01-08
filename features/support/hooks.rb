0729ae53e7887afb816d03813fb5b57c3260a7cc
johannes112 <johannes.emmos@gmail.com>
2019-01-08 11:10:59 +0100

    try improvement: do not set status of test result within code -> use jenkins-plugin

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -13,20 +13,20 @@ After ('@prestep') do |s|
 end
 
 #with this around there will be a video for each scenario but this takes too much time
-if ENV['DRIVER'] == 'saucelabs'
-  Around do |scenario, block|
-    block.call
-    #get id of job
-    sessionid = ::Capybara.current_session.driver.browser.session_id
-    ::Capybara.current_session.driver.quit
-    #get last tag-name
-    scenario_tag = scenario.source_tag_names.last
-    env_vars = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']} #{ENV['JOB_NAME']}__#{ENV['BUILD_NUMBER']}"
-    jobname = "#{env_vars} - #{scenario_tag}"
-    # jobname = "#{scenario.feature.name} - #{scenario.name} - #{scenario_tag}"
-    job = SauceWhisk::Jobs.fetch sessionid
-    job.name = jobname
-    job.passed = scenario.passed? ? true : false
-    job.save
-  end
-end
+# if ENV['DRIVER'] == 'saucelabs'
+#   Around do |scenario, block|
+#     block.call
+#     #get id of job
+#     sessionid = ::Capybara.current_session.driver.browser.session_id
+#     ::Capybara.current_session.driver.quit
+#     #get last tag-name
+#     scenario_tag = scenario.source_tag_names.last
+#     env_vars = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']} #{ENV['JOB_NAME']}__#{ENV['BUILD_NUMBER']}"
+#     jobname = "#{env_vars} - #{scenario_tag}"
+#     # jobname = "#{scenario.feature.name} - #{scenario.name} - #{scenario_tag}"
+#     job = SauceWhisk::Jobs.fetch sessionid
+#     job.name = jobname
+#     job.passed = scenario.passed? ? true : false
+#     job.save
+#   end
+# end
