6b465f4dc778f3740a766e862cffa4b4eaed01bd
johannes112 <johannes.emmos@gmail.com>
2019-01-11 15:34:38 +0100

    fix: set real status of job -> changed 'around'hook to 'after'-hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -14,8 +14,22 @@ end
 
 #with this around there will be a video for each scenario but this takes too much time
 if ENV['DRIVER'] == 'saucelabs'
-  Around do |scenario, block|
-    block.call
+  # Around do |scenario, block|
+  #   block.call
+  #   #get id of job
+  #   sessionid = ::Capybara.current_session.driver.browser.session_id
+  #   ::Capybara.current_session.driver.quit
+  #   #get last tag-name
+  #   scenario_tag = scenario.source_tag_names.last
+  #   env_vars = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']} #{ENV['JOB_NAME']}__#{ENV['BUILD_NUMBER']}"
+  #   jobname = "#{env_vars} - #{scenario_tag}"
+  #   # jobname = "#{scenario.feature.name} - #{scenario.name} - #{scenario_tag}"
+  #   job = SauceWhisk::Jobs.fetch sessionid
+  #   job.name = jobname
+  #   job.passed = scenario.passed? ? true : false
+  #   job.save
+  # end
+  After do |scenario|
     #get id of job
     sessionid = ::Capybara.current_session.driver.browser.session_id
     ::Capybara.current_session.driver.quit
@@ -24,9 +38,12 @@ if ENV['DRIVER'] == 'saucelabs'
     env_vars = "Shopware tests: #{ENV['BROWSER']} #{ENV['SYSTEM']} #{ENV['SHOP']} #{ENV['COUNTRY']} #{ENV['JOB_NAME']}__#{ENV['BUILD_NUMBER']}"
     jobname = "#{env_vars} - #{scenario_tag}"
     # jobname = "#{scenario.feature.name} - #{scenario.name} - #{scenario_tag}"
+    # Create a job with details fetched from the API
     job = SauceWhisk::Jobs.fetch sessionid
-    job.name = jobname
-    job.passed = scenario.passed? ? true : false
-    job.save
+    job_id = job.id      # Sauce job_id
+    job_status = scenario.passed? ? true : false
+    SauceWhisk::Jobs.pass_job job_id
+    SauceWhisk::Jobs.fail_job job_id
+    SauceWhisk::Jobs.change_status job_id, job_status
   end
 end
